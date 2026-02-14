16df897fc0f76fc9687400de5fc57c746e5cefc4
Nabil Sahsada Suratno <nabilsahsadabisnis@gmail.com>
2026-02-14 15:49:32 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -146,36 +146,14 @@ return [
 
     'client' => env('REDIS_CLIENT', 'phpredis'),
 
-    'options' => [
-        'cluster' => env('REDIS_CLUSTER', 'redis'),
-        'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
-        'persistent' => env('REDIS_PERSISTENT', false),
-    ],
-
     'default' => [
         'url' => env('REDIS_URL'),
-        'host' => env('REDIS_HOST'),
-        'username' => env('REDIS_USERNAME'),
-        'password' => env('REDIS_PASSWORD'),
-        'port' => (int) env('REDIS_PORT', 6379),
-        'database' => (int) env('REDIS_DB', 0),
-        'max_retries' => env('REDIS_MAX_RETRIES', 3),
-        'backoff_algorithm' => env('REDIS_BACKOFF_ALGORITHM', 'decorrelated_jitter'),
-        'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
-        'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
+        'database' => 0,
     ],
 
     'cache' => [
         'url' => env('REDIS_URL'),
-        'host' => env('REDIS_HOST'),
-        'username' => env('REDIS_USERNAME'),
-        'password' => env('REDIS_PASSWORD'),
-        'port' => (int) env('REDIS_PORT', 6379),
-        'database' => (int) env('REDIS_CACHE_DB', 1),
-        'max_retries' => env('REDIS_MAX_RETRIES', 3),
-        'backoff_algorithm' => env('REDIS_BACKOFF_ALGORITHM', 'decorrelated_jitter'),
-        'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
-        'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
+        'database' => 1,
     ],
 
 ],
